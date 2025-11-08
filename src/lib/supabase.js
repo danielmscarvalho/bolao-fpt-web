531732d96f42aa7679a4006ed6c87df282a336a4
Daniel Carvalho <daniel@bolao-fpt.com>
2025-11-07 23:05:04 -0500

    fix: Atualizar URL e credenciais do Supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zjnkzoqxgjqwwcndsmba.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqbmt6b3F4Z2pxd3djbmRzbWJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxOTA2NjksImV4cCI6MjA3Nzc2NjY2OX0.E2PHqYyOnPlbKGKYSv0y9f-fx2aNj-RO_-R2FMTU5N0'
+const supabaseUrl = 'https://zfabxlsycklxfglhjcap.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmYWJ4bHN5Y2tseGZnbGhqY2FwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NTMwMzksImV4cCI6MjA3ODAyOTAzOX0.hJwFmenqbNs-MUpzIPgHpYMr_YBkjB8EVZVVq-lR7gs'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
